fdb2899b0038c425f6bf5a43d53c7a379b4ecca2
Marco-Emad <marcoemad42@yahoo.com>
2024-09-05 23:59:34 +0300

    Payment_onDelete_NoAction

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Fashion_Flex.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
