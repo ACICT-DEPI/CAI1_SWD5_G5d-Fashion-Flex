7e599be7bb27a2f6e0714ccf92f38458147d6e7f
ziadhassan7 <ziadhassan7@gmail.com>
2024-09-09 00:46:36 +0300

    feat(Order/OrderItem Controllers)

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,77 @@
+﻿using Fashion_Flex.Models;
+using Fashion_Flex.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fashion_Flex.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderController(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var orders = _orderRepository.GetAll();
+            return View(orders);
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SaveNewOrder(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                _orderRepository.Add(order);
+                _orderRepository.Save();
+                return RedirectToAction("Index");
+            }
+
+            return View("New", order);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var order = _orderRepository.GetById(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return View(order);
+        }
+
+        [HttpPost]
+        public IActionResult SaveEditedOrder(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                _orderRepository.Update(order);
+                _orderRepository.Save();
+                return RedirectToAction("Index");
+            }
+
+            return View("Edit", order);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var order = _orderRepository.GetById(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _orderRepository.Delete(id);
+            _orderRepository.Save();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
