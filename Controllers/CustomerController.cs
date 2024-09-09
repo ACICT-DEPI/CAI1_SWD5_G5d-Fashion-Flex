4ad0304651a7780a3e0eeb617388434167312ce6
Marco-Emad <marcoemad42@yahoo.com>
2024-09-09 20:04:31 +0300

    build-CustomerController

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using Fashion_Flex.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fashion_Flex.Controllers
+{
+    public class CustomerController : Controller
+    {        
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerController(ICustomerRepository _customerRepository)
+        {
+            this._customerRepository = _customerRepository;
+        }
+        public IActionResult Index()
+        {
+            return View(_customerRepository.GetAll());
+        }
+    }
+}
