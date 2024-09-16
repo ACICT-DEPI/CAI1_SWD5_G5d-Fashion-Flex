a5ac600abd7eb7407ac93e4d03f37b238f2b7e72
Omnia-Rowezak <o.rowezak@yahoo.com>
2024-09-16 15:59:45 +0300

    crud operation

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,22 +1,106 @@
-﻿using Fashion_Flex.Repository;
+﻿using Fashion_Flex.IRepositories;
+using Fashion_Flex.Models;
+using Fashion_Flex.Repository;
 using Microsoft.AspNetCore.Mvc;
 
+
+//Start
 namespace Fashion_Flex.Controllers
 {
+    //public class ProductController : Controller
+    //{
+
+    //        private readonly IProductRepository _productRepository;
+
+    //        public ProductController(IProductRepository _productRepository)
+    //        {
+    //            this._productRepository = _productRepository;
+    //        }
+    //        public IActionResult Index()
+    //        {
+    //            return View(_productRepository.GetAll());
+    //        }
+
+    //}
+
+
     public class ProductController : Controller
     {
+        private readonly IProductRepository _productRepository;
 
-            private readonly ICustomerRepository _productRepository;
-
-            public ProductController(ICustomerRepository _productRepository)
-            {
-                this._productRepository = _productRepository;
-            }
-            public IActionResult Index()
-            {
-                return View(_productRepository.GetAll());
-            }
-
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
         }
-    
+
+        public IActionResult Index()
+        {
+            return View(_productRepository.GetAll());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = _productRepository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productRepository.Add(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var product = _productRepository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productRepository.Update(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _productRepository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _productRepository.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+
 }
