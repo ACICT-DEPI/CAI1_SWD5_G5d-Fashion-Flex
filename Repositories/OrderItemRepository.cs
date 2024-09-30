0475ae758bdc67f7305fa2c54d4fd5817a14d1e4
Marco-Emad <marcoemad42@yahoo.com>
2024-09-30 21:06:12 +0300

    feat(Shopping Cart)

--- Repositories/OrderItemRepository.cs
+++ Repositories/OrderItemRepository.cs
@@ -3,48 +3,52 @@ using Fashion_Flex.Repository;
 
 namespace Fashion_Flex.IRepositories.Repository
 {
-    public class OrderItemRepository : IOrderItemRepository
-    {
-        private readonly FFContext context;
+	public class OrderItemRepository : IOrderItemRepository
+	{
+		private readonly FFContext context;
 
-        public OrderItemRepository(FFContext context)
-        {
-            this.context = context;
-        }
+		public OrderItemRepository(FFContext context)
+		{
+			this.context = context;
+		}
 
-        public void Add(Order_Item obj)
-        {
-            context.Order_Items.Add(obj);
-        }
-        public void Update(Order_Item obj)
-        {
-            context.Order_Items.Update(obj);
-        }
-        public void Delete(int id)
-        {
-            var item = GetById(id);
-            context.Order_Items.Remove(item);
-        }
-        public Order_Item GetById(int id)
-        {
-            return context.Order_Items.FirstOrDefault(d => d.Id == id);
-        }
-        public List<Order_Item> GetAll()
-        {
-            return context.Order_Items.ToList();
-        }
+		public void Add(Order_Item obj)
+		{
+			context.Order_Items.Add(obj);
+		}
+		public void Update(Order_Item obj)
+		{
+			context.Order_Items.Update(obj);
+		}
+		public void Delete(int id)
+		{
+			var item = GetById(id);
+			context.Order_Items.Remove(item);
+		}
+		public Order_Item GetById(int id)
+		{
+			return context.Order_Items.FirstOrDefault(d => d.Id == id);
+		}
+		public List<Order_Item> GetAll()
+		{
+			return context.Order_Items.ToList();
+		}
 		public bool OrderItemExist(int orderId, int productId)
-        {
-            return context.Order_Items.Where(i => i.Order_Id==orderId && i.Product_Id==productId).Any();
-        }
+		{
+			return context.Order_Items.Where(i => i.Order_Id == orderId && i.Product_Id == productId).Any();
+		}
 		public Order_Item GetByProductAndOrderId(int orderId, int productId)
-        {
+		{
 			var item = context.Order_Items.Where(i => i.Order_Id == orderId && i.Product_Id == productId).FirstOrDefault();
-            return item;
+			return item;
+		}
+		public List<Order_Item> GetByOrderId(int orderId)
+		{
+			return context.Order_Items.Where(i => i.Order_Id == orderId).ToList();
 		}
 		public void Save()
-        {
-            context.SaveChanges();
-        }
-    }
+		{
+			context.SaveChanges();
+		}
+	}
 }
