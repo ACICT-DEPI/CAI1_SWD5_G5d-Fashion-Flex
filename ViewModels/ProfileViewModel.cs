571d66cfa8223ff9b14e0e7028725138f30ee946
Marco-Emad <marcoemad42@yahoo.com>
2024-10-04 16:46:05 +0300

    change(Payment&Customer Schema)

--- ViewModels/ProfileViewModel.cs
+++ ViewModels/ProfileViewModel.cs
@@ -2,20 +2,23 @@
 
 namespace Fashion_Flex.ViewModels
 {
-    public class ProfileViewModel
-    {
-        public int Id { get; set; }
-        [Display(Name ="First Name")]
-        public string First_Name { get; set; }
+	public class ProfileViewModel
+	{
+		public int Id { get; set; }
+		[Display(Name = "First Name")]
+		public string First_Name { get; set; }
 		[Display(Name = "Last Name")]
 		public string Last_Name { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }
+		public string Email { get; set; }
+		public string Street_Name { get; set; }
+		public string Building_No { get; set; }
+		public string City { get; set; }
+		public string Governorate { get; set; }		
 		[Display(Name = "Date of Birth")]
 		public DateOnly Date_Of_Birth { get; set; }
 		[Display(Name = "Phone Number")]
 		public string Phone_Number { get; set; }
-        public string Phone_Country_Code { get; set; }
-        public bool Is_Active { get; set; }        
-    }
+		public string Phone_Country_Code { get; set; }
+		public bool Is_Active { get; set; }
+	}
 }
--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -2,47 +2,53 @@
 
 namespace Fashion_Flex.ViewModels
 {
-    public class RegisterViewModel
-    {
-        // Identity Fields
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
+	public class RegisterViewModel
+	{
+		// Identity Fields
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
+		[Required]
+		[DataType(DataType.Password)]
+		[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+		public string ConfirmPassword { get; set; }
 
-        [Phone]
-        [Display(Name = "Phone Number")]
-        public string? PhoneNumber { get; set; }
+		[Phone]
+		[Display(Name = "Phone Number")]
+		public string? PhoneNumber { get; set; }
 
-        // Customer Fields
-        [Required]
-        [Display(Name = "First Name")]
-        public string First_Name { get; set; }
+		// Customer Fields
+		[Required]
+		[Display(Name = "First Name")]
+		public string First_Name { get; set; }
 
-        [Required]
-        [Display(Name = "Last Name")]
-        public string Last_Name { get; set; }
+		[Required]
+		[Display(Name = "Last Name")]
+		public string Last_Name { get; set; }
 
-        [Required]
-        [DataType(DataType.Date)]
-        [Display(Name = "Date of Birth")]
-        public DateOnly Date_Of_Birth { get; set; }
+		[Required]
+		[DataType(DataType.Date)]
+		[Display(Name = "Date of Birth")]
+		public DateOnly Date_Of_Birth { get; set; }
 
-        [Required]
-        public string Address { get; set; }
+		[Required]
+		public string Street_Name { get; set; }
+		[Required]
+		public string Building_No { get; set; }
+		[Required]
+		public string City { get; set; }
+		[Required]
+		public string Governorate { get; set; }
 
-        [Display(Name = "Phone Country Code")]
-        public string? Phone_Country_Code { get; set; }
+		[Display(Name = "Phone Country Code")]
+		public string? Phone_Country_Code { get; set; }
 
-        public bool Is_Active { get; set; } = true; // Default is active for new customers
+		public bool Is_Active { get; set; } = true; // Default is active for new customers
 
-    }
+	}
 }
