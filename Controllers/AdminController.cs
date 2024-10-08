75944051388df55156e69a093935539aac3c9e78
Marco-Emad <marcoemad42@yahoo.com>
2024-10-08 11:37:34 +0300

    establishing dashboard layout

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Fashion_Flex.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fashion_Flex.Controllers
+{
+	public class AdminController : Controller
+	{
+		private readonly ICustomerRepository _customerRepository;
+		public AdminController(ICustomerRepository _customerRepository)
+		{
+			this._customerRepository = _customerRepository;
+		}
+		public IActionResult Index()
+		{
+			ViewData["currTab"] = "dashboard";
+			return View();
+		}
+
+		public IActionResult Customers()
+		{
+			ViewData["currTab"] = "customers";
+			var customers = _customerRepository.GetAll();
+			return View(customers);
+		}
+		public IActionResult Products()
+		{
+			ViewData["currTab"] = "products";
+			return View();
+		}
+		public IActionResult Orders()
+		{
+			ViewData["currTab"] = "orders";
+			return View();
+		}
+	}
+}
