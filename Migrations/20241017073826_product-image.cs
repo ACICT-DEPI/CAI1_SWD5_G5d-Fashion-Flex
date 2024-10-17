128b0243de3e0a420344b8229db2281ef635542e
Marco-Emad <marcoemad42@yahoo.com>
2024-10-17 10:45:08 +0300

    fix product-image migration

---
+++ Migrations/20241017073826_product-image.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Fashion_Flex.Migrations
+{
+    /// <inheritdoc />
+    public partial class productimage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Products");
+
+        }
+    }
+}
--- Models/Product.cs
+++ Models/Product.cs
@@ -2,25 +2,25 @@
 
 namespace Fashion_Flex.Models
 {
-    public class Product
-    {
-        public int Id { get; set; }        
-        public string Name { get; set; }
-        public string? Image {  get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public int Available_Quantity { get; set; }
-        public string Category { get; set; }
-        public string Color { get; set; }
-        public double Discount { get; set; }
-        public DateTime Added_Date { get; set; }
-        public string type { get; set; }
+	public class Product
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string? Image { get; set; }
+		public string Description { get; set; }
+		public decimal Price { get; set; }
+		public int Available_Quantity { get; set; }
+		public string Category { get; set; }
+		public string Color { get; set; }
+		public double Discount { get; set; }
+		public DateTime Added_Date { get; set; }
+		public string type { get; set; }
 
-        //Order_Item OM--refersTo-->|1 Product
-        public virtual List<Order_Item>? Order_Items { get; set; }
-        //Product |1--Recevice-->OM Review
-        public virtual List<Review>? Reviews { get; set; }      
-        //Product |1--Favoried-->OM User_Favorites
-        public virtual List<FavoriteList>? User_Favorites { get; set; }
-    }
+		//Order_Item OM--refersTo-->|1 Product
+		public virtual List<Order_Item>? Order_Items { get; set; }
+		//Product |1--Recevice-->OM Review
+		public virtual List<Review>? Reviews { get; set; }
+		//Product |1--Favoried-->OM User_Favorites
+		public virtual List<FavoriteList>? User_Favorites { get; set; }
+	}
 }
